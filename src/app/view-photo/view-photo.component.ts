6fa16eaafbcf066df109a8ecd7721b50a1222c3a
Ivan <fridey0@gmail.com>
2021-06-01 10:46:42 +0300

    Добавил кнопки шаринга в соц.сети

--- src/app/view-photo/view-photo.component.ts
+++ src/app/view-photo/view-photo.component.ts
@@ -1,6 +1,8 @@
+import { startWith, switchMap, takeUntil } from 'rxjs/operators';
 import { DataService } from './../services/data.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'app-view-photo',
@@ -11,6 +13,30 @@ export class ViewPhotoComponent implements OnInit {
 
     query = this.activatedRoute.snapshot.params.id;
     dataImage: any = {};
+    shareBtn = [
+        {
+            type: 'vk',
+            title: 'test-vk',
+            description: 'description-vk'
+        },
+        {
+            type: 'facebook',
+            title: 'title-facebook',
+            description: 'description-facebook'
+        },
+        {
+            type: 'telegram',
+            title: 'title-telegram',
+            description: 'telegram-description'
+        },
+        {
+            type: 'twitter',
+            title: 'title-twitter',
+            description: 'twitter-description'
+        }
+    ];
+
+    private ngUnsubscribe = new Subject();
 
     constructor(
         private dataService: DataService,
@@ -18,7 +44,10 @@ export class ViewPhotoComponent implements OnInit {
     ) { }
 
     ngOnInit(): void {
-        this.dataService.getPhotoById(this.query).subscribe(res => {
+        this.activatedRoute.params.pipe(
+            switchMap(params => this.dataService.getPhotoById(params.id)),
+            takeUntil(this.ngUnsubscribe),
+        ).subscribe(res => {
             console.log(res);
             this.dataImage = res;
         });
@@ -28,7 +57,7 @@ export class ViewPhotoComponent implements OnInit {
         const photo = [];
         this.dataImage.related_collections?.results.forEach(el => {
             el.preview_photos.forEach(p => {
-                if (photo.length < 8) {
+                if (photo.length < 8 && p.id !== this.dataImage.id) {
                     photo.push(p);
                 }
             });
--- src/app/view-photo/view-photo.module.ts
+++ src/app/view-photo/view-photo.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ViewPhotoComponent } from './view-photo.component';
+import { ShareButtonModule } from 'ngx-sharebuttons/button';
+import { ShareIconsModule } from 'ngx-sharebuttons/icons';
+
 
 @NgModule({
     declarations: [ViewPhotoComponent],
     imports: [
         CommonModule,
+        ShareIconsModule,
+        ShareButtonModule,
         RouterModule.forChild([
             { path: ':id', component: ViewPhotoComponent }
         ])
